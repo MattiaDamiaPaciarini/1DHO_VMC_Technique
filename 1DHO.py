eb987261cf881ccfae75b2901bff1f4e46dcc824
MattiaDamiaPaciarini <mattiadamiapaciarini@gmail.com>
2021-10-21 11:47:01 +0200

    Update 1DHO.py

--- 1DHO.py
+++ 1DHO.py
@@ -6,15 +6,17 @@ from matplotlib import pyplot as plt
 
 NM = 10000 #Number of Moves
 i = 0
-list_Y = []
+list_Y = [] 
 list_X = []
 list_Err = []
 
-for j in range(800, 1700, 15):
+# Energy Computation
+
+for j in range(800, 1700, 15): # Range of the parameter alpha (= j/1000) 
   E = 0
   while (i<NM):
-    x = random.gauss(0,1)/math.sqrt(2)/(j/1000)
-    LE = (j/1000)**2-(j/1000)**4*x**2-5*math.exp(-x**2)
+    x = random.gauss(0,1)/math.sqrt(2)/(j/1000) # Positions extracted from a gaussian
+    LE = (j/1000)**2-(j/1000)**4*x**2-5*math.exp(-x**2) # Local Energy operator
     E = E+LE
     SE = E**2+LE**2 
     i = i+1
@@ -23,10 +25,13 @@ for j in range(800, 1700, 15):
   list_X.append(j/1000)
   list_Err.append(math.sqrt((SE/NM)-(E/NM)**2)/NM)
 
+# Plotting the results
 
 plt.errorbar(list_X, list_Y, yerr = list_Err, fmt='o', color= 'black')
 plt.xlabel('alpha')
 plt.ylabel('Mean Energy')
 plt.title('Energy wrt alpha')
 plt.show()
+
+
 
